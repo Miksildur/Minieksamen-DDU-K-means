7c638d3e9d8858ed77011e591b7c01b57db9f735
Miksildur <miksildur@gmail.com>
2025-02-27 15:00:17 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 st.title("Testing Streamlit")
 st.write("**pls work O_O**")
